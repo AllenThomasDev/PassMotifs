3900dfe17423bd043a420aab33028ff58cb664e6
Allen Thomas <allen.thomas.dev@gmail.com>
2023-07-18 23:11:14 +0530

    Generate flow and flow labels from pass sequences

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 from statsbombpy import sb
 import pandas as pd
 from utils import *
+import pprint as pp
+
 
 def main():
-  """Calculates passflow motifs from a list of passes.
+    """Calculates passflow motifs from a list of passes.
 
-  Args:
-    passes: A list of passes, where each pass is a tuple of (sender, receiver).
-    motif_length: The length of the motifs to calculate.
+    Args:
+      passes: A list of passes, where each pass is a tuple of (sender, receiver).
+      motif_length: The length of the motifs to calculate.
 
-  Returns:
-    A dictionary of passflow motifs, where the key is the motif and the value
-    is the number of times the motif appears.
-  """
-  threshold = 5
-  df = sb.events(match_id=15986, split=True, flatten_attrs=True)["passes"]
-  team_1_records, team_2_records = get_team_records(df)
-  team_1_pass_sequences = create_possession_sequences_from_pass_events(team_1_records, threshold)
-  team_2_pass_sequences = create_possession_sequences_from_pass_events(team_2_records, threshold)
-  print(len(team_1_pass_sequences),len(team_2_pass_sequences))
+    Returns:
+      A dictionary of passflow motifs, where the key is the motif and the value
+      is the number of times the motif appears.
+    """
+    threshold = 5
+    df = sb.events(match_id=15986, split=True, flatten_attrs=True)["passes"]
+    team_1_records, team_2_records = get_team_records(df)
+    team_1_pass_sequences = create_possession_sequences_from_pass_events(
+        team_1_records, threshold)
+    team_2_pass_sequences = create_possession_sequences_from_pass_events(
+        team_2_records, threshold)
+    three_passes = extract_three_pass_sub_possessions(team_1_pass_sequences[111])
+    for sequence in three_passes:
+        flow = get_flow_for_sequence(sequence)
+        print(flow, convert_pass_flow(flow))
+
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 def get_team_records(df):
     team1, team2 = list(df['team'].unique())
     df['mod_time'] = df['minute']*60 + df['second']
-    pass_columns = ['location', 'player', 'pass_recipient',
+    pass_columns = ['player', 'pass_recipient', 'location',
                     'pass_length', 'pass_end_location', 'timestamp', 'mod_time']
     return df[df['team'] == team1][pass_columns].to_dict('records'), df[df['team'] == team2][pass_columns].to_dict('records')
 
@@ -20,3 +20,58 @@ def create_possession_sequences_from_pass_events(records, threshold=5):
             current_sublist = [record]
     sublists.append(current_sublist)
     return sublists
+
+
+def extract_three_pass_sub_possessions(passes):
+    """Extracts all the three-pass long sub-possessions from the ball possession.
+
+    Args:
+      passes: A list of passes.
+
+    Returns:
+      A list of three-pass long sub-possessions.
+    """
+
+    sub_possessions = []
+    for i in range(len(passes)):
+        if i + 2 < len(passes):
+            sub_possession = passes[i:i + 3]
+            sub_possessions.append(sub_possession)
+    return sub_possessions
+
+
+def get_flow_for_sequence(sub_possession):
+    """Converts the player identifiers in the sub-possession to A, B, C, and D labels.
+
+    Args:
+      sub_possession: A three-pass long sub-possession.
+
+    Returns:
+      A list of A, B, C, and D labels.
+    """
+    flow = []
+    origin_player = sub_possession[0]['player']
+    flow.append(origin_player)
+    for entry in sub_possession:
+        flow.append(entry['pass_recipient'])
+    return flow
+
+
+def convert_pass_flow(pass_flow):
+    """Converts the pass flow to A->B->C->B.
+
+    Args:
+      pass_flow: The pass flow.
+
+    Returns:
+      The pass flow in A->B->C->B format.
+    """
+    flow_string = ''
+    # maintain order while dropping dupes
+    players = list(dict.fromkeys(pass_flow))
+    labels = ['A', 'B', 'C', 'D'][:len(players)]
+    players_to_labels = dict(zip(players, labels))
+    converted_pass_flow = [players_to_labels[player] for player in pass_flow]
+    for label in converted_pass_flow:
+        flow_string+=label
+    return flow_string
